21d40a963313c06a7803dbb15b9ab5a80cbdaadb
Juan P Hernandez <juanphdez@gmail.com>
2015-02-24 21:06:34 -0800

    commit

---
+++ HelloMyName/src/HelloMyName.java
@@ -0,0 +1,13 @@
+// HelloMyName.java
+// Printing lines
+
+public class HelloMyName 
+{
+	// main method
+	public static void main (String[] args)
+	{
+		System.out.println("Hello\nMy name is Juan P Hernandez");
+
+	}
+
+}
