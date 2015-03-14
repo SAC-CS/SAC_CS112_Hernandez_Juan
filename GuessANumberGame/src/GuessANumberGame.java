8aaaaf34f99c661b3f16ab94166dd4c8c85d9962
Juan P Hernandez <juanphdez@gmail.com>
2015-03-13 21:55:08 -0700

    update

--- GuessANumberGame/src/GuessANumberGame.java
+++ GuessANumberGame/src/GuessANumberGame.java
@@ -11,28 +11,45 @@ public class GuessANumberGame
 		// Variables
 		int computerNum;
 		int playerNum;
+		int playerWins = 0;
+		int computerWins = 0;
+		int totalGames = 0;
+		float playerPercentage = 0;
 		String answer = "Y";
-		computerNum = 0 + (int) (Math.random() * 10);
 		
 		Scanner input = new Scanner (System.in);
 		
-		// User input
-		System.out.print("Enter a number between 0 and 10: ");
-		playerNum = input.nextInt();
-		
-		// Process
 		do
 		{
+			
+			computerNum = 0 + (int) (Math.random() * 10);
+			// User input
+			System.out.print("Enter a number between 0 and 10: ");
+			playerNum = input.nextInt();
+			
+			// Process
 			if ((playerNum < 0) || (playerNum > 10))
 				System.out.println("You entered an invalid number.");
 			else
 			{
 				if (playerNum == computerNum)
+				{
 					System.out.println("Correct, You Win!!");
+					playerWins++;
+					totalGames++;
+				}
 				else if (playerNum > computerNum)
+				{
 					System.out.println("Number is too big!");
+					computerWins++;
+					totalGames++;
+				}
 				else
+				{
 					System.out.println("Number is too small!");
+					computerWins++;
+					totalGames++;
+				}
 			}
 		
 			System.out.print("\nWould you like to play again? ");
@@ -40,6 +57,15 @@ public class GuessANumberGame
 		
 		}while(answer.equalsIgnoreCase("Y"));
 		
-
+		
+		// output
+		System.out.println("Total good guesses: " + playerWins);
+		System.out.println("Total bad guesses: " + computerWins);
+		System.out.println("Total games played: " + totalGames);
+		
+		playerPercentage = ((float) playerWins / totalGames)  * 100;
+		
+		System.out.printf("Player win percentage: %.2f%s", playerPercentage, "%");
+		
 	}
 }
