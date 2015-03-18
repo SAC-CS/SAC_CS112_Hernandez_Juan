5ebe994125cfd11d0dcc0d807de218d2b42f05dc
Juan P Hernandez <juanphdez@gmail.com>
2015-03-17 21:29:01 -0700

    Lab 4

--- GuessANumberGame/src/GuessANumberGame.java
+++ GuessANumberGame/src/GuessANumberGame.java
@@ -1,7 +1,7 @@
 // Juan P Hernandez
 // Guess a number game
 
-import java.util.Scanner;
+import javax.swing.JOptionPane;
 
 public class GuessANumberGame 
 {
@@ -10,62 +10,63 @@ public class GuessANumberGame
 		
 		// Variables
 		int computerNum;
+		String playerNumber;
+		String outputMessage;
 		int playerNum;
 		int playerWins = 0;
 		int computerWins = 0;
 		int totalGames = 0;
 		float playerPercentage = 0;
-		String answer = "Y";
+		String answer;
 		
-		Scanner input = new Scanner (System.in);
+		
 		
 		do
 		{
 			computerNum = 0 + (int) (Math.random() * 10);
 			// User input
-			System.out.print("Enter a number between 0 and 10: ");
-			playerNum = input.nextInt();
+			playerNumber = JOptionPane.showInputDialog("Enter a number between 0 and 10");
+			playerNum = Integer.parseInt(playerNumber);
 			
 			// Process
 			if ((playerNum < 0) || (playerNum > 10))
 			{
-				System.out.println("You entered an invalid number.");
+				JOptionPane.showMessageDialog(null, "You entered an invalid number");
 			}
 			else
 			{
 				if (playerNum == computerNum)
 				{
-					System.out.println("Correct, You Win!!");
+					JOptionPane.showMessageDialog(null, "Correct, you win");
 					playerWins++;
 					totalGames++;
 				}
 				else if (playerNum > computerNum)
 				{
-					System.out.println("Number is too big!");
+					JOptionPane.showMessageDialog(null, "Number is too big!");
 					computerWins++;
 					totalGames++;
 				}
 				else
 				{
-					System.out.println("Number is too small!");
+					JOptionPane.showMessageDialog(null, "Number is too small!");
 					computerWins++;
 					totalGames++;
 				}
 			}
-			System.out.print("Do you want to play again? Enter Yes or No: ");
-			answer = input.next();
+			answer = JOptionPane.showInputDialog("Do you want to play again? Enter Yes or No: ");	
 			
 		}while (answer.equalsIgnoreCase("YES"));
 		
 		
 		// output
-		System.out.println("\nTotal good guesses: " + playerWins);
-		System.out.println("Total bad guesses: " + computerWins);
-		System.out.println("Total games played: " + totalGames);
 		
-		playerPercentage = ((float) playerWins / totalGames)  * 100;
+		outputMessage = String.format("Total good guesses: %d\nTotal bad guesses: %d\nTotal games played: %d", playerWins, computerWins, totalGames);
+
+		JOptionPane.showMessageDialog(null, outputMessage);
+		//playerPercentage = ((float) playerWins / totalGames)  * 100;
 		
-		System.out.printf("Player win percentage: %.2f%s", playerPercentage, "%");
+		//System.out.printf("Player win percentage: %.2f%s", playerPercentage, "%");
 		
 	}
 }// 
