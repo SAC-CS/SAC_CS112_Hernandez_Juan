300688a4cd55a0b0b1d6c72bcd7cbc31b9193f0b
Juan P Hernandez <juanphdez@gmail.com>
2015-03-22 18:02:54 -0700

    HW06

--- RockPaperScissor/src/RockPaperScissor.java
+++ RockPaperScissor/src/RockPaperScissor.java
@@ -1,6 +1,6 @@
 // Juan P Hernandez
 // Program: Rock Paper Scissors
-//computerPlayer = 1 + (int) (Math.random() * 3);
+//
 // 03/07/2015
 
 import java.util.Scanner;
@@ -14,36 +14,42 @@ public class RockPaperScissor
 		
 		int userPlayer;
 		int computerPlayer;
-		
-		
-		computerPlayer = 2;
-		
-		
-		// User input
-		System.out.println("Player, enter 1 for Rock, 2 for Paper, or 3 for Scissors: ");
-		userPlayer = input.nextInt();
+		String answer;
 		
 		// Process
-		if ( userPlayer == computerPlayer)
-			System.out.println("It's a tie!");
+		do
+		{
+			
+			System.out.print("Player, enter 1 for Rock, 2 for Paper, or 3 for Scissors: ");
+			userPlayer = input.nextInt();
+			
+			computerPlayer = 1 + (int) (Math.random() * 3);
+			
+			if ( userPlayer == computerPlayer)
+				System.out.println("It's a tie!");
 		
-		else if ( userPlayer == 1 && computerPlayer == 2)
-			System.out.println("Paper beats Rock, Computer Wins!!");
+			else if ( userPlayer == 1 && computerPlayer == 2)
+				System.out.println("Paper beats Rock, Computer Wins!!");
 		
-		else if (userPlayer == 1 && computerPlayer == 3)
-			System.out.println("Rock beats Scissors, You Win!!");
+			else if (userPlayer == 1 && computerPlayer == 3)
+				System.out.println("Rock beats Scissors, You Win!!");
 		
-		else if (userPlayer == 2 && computerPlayer == 1)
-			System.out.println("Paper beats Rock, You Win!!");
+			else if (userPlayer == 2 && computerPlayer == 1)
+				System.out.println("Paper beats Rock, You Win!!");
 		
-		else if (userPlayer == 2 && computerPlayer == 3)
-			System.out.println("Scissors beat Paper, Computer Wins!!");
+			else if (userPlayer == 2 && computerPlayer == 3)
+				System.out.println("Scissors beat Paper, Computer Wins!!");
 		
-		else if (userPlayer == 3 && computerPlayer == 1)
-			System.out.println("Rock beats Scissors, Computer Wins!!");
+			else if (userPlayer == 3 && computerPlayer == 1)
+				System.out.println("Rock beats Scissors, Computer Wins!!");
 		
-		else if (userPlayer == 3 && computerPlayer == 2)
-			System.out.println("Scissors beat Paper, You Win!!");
+			else if (userPlayer == 3 && computerPlayer == 2)
+				System.out.println("Scissors beat Paper, You Win!!");
+		
+			System.out.print("Do you want to play again, Y or N?: ");
+			answer = input.next();
+		
+		}while(answer.equalsIgnoreCase("Y"));
 				
 	}
 }
