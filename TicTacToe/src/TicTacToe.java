46f488984637dea0cce46b8c9ca7f9e8fabb9bd5
Juan P Hernandez <juanphdez@gmail.com>
2015-04-11 20:41:33 -0700

    update

--- TicTacToe/src/TicTacToe.java
+++ TicTacToe/src/TicTacToe.java
@@ -20,8 +20,10 @@ public class TicTacToe
 		{
 			
 			// Player A input dialog message
-			outputMessageA = String.format("Let's play tic-tac-toe\n%s   %s   %s\n%s   %s   %s\n%s   %s   %s\nPlayer A:",ticArray[0], ticArray[1], ticArray[2],
-					ticArray[3], ticArray[4], ticArray[5], ticArray[6], ticArray[7], ticArray[8]);
+			outputMessageA = String.format("Let's play tic-tac-toe\n%s   %s   %s\n%s   %s   %s\n%s   %s   %s\nPlayer A:", 
+					ticArray[0], ticArray[1], ticArray[2],
+					ticArray[3], ticArray[4], ticArray[5], 
+					ticArray[6], ticArray[7], ticArray[8]);
 				
 			playerAChoice = JOptionPane.showInputDialog(null, outputMessageA);
 			playCount++;
@@ -50,10 +52,14 @@ public class TicTacToe
 			}
 			
 			// check to see if player A is a winner
-			if((ticArray[0] == "X" && ticArray[0] == ticArray[1] && ticArray[1] == ticArray[2]) || (ticArray[3] == "X" && ticArray[3] == ticArray[4] && ticArray[4] == ticArray[5]) 
-					|| (ticArray[6] == "X" && ticArray[6] == ticArray[7] && ticArray[7] == ticArray[8]) || (ticArray[0] == "X" && ticArray[0] == ticArray[3] && ticArray[3] == ticArray[6]) 
-					|| (ticArray[1] == "X" && ticArray[1] == ticArray[4] && ticArray[4] == ticArray[7]) || (ticArray[2] == "X" && ticArray[2] == ticArray[5] && ticArray[5] == ticArray[8])
-					|| (ticArray[0] == "X" && ticArray[0] == ticArray[4] && ticArray[4] == ticArray[8]) || (ticArray[2] == "X" && ticArray[2] == ticArray[4] && ticArray[4] == ticArray[6]))
+			if((ticArray[0] == "X" && ticArray[0] == ticArray[1] && ticArray[1] == ticArray[2]) 
+					|| (ticArray[3] == "X" && ticArray[3] == ticArray[4] && ticArray[4] == ticArray[5]) 
+					|| (ticArray[6] == "X" && ticArray[6] == ticArray[7] && ticArray[7] == ticArray[8]) 
+					|| (ticArray[0] == "X" && ticArray[0] == ticArray[3] && ticArray[3] == ticArray[6]) 
+					|| (ticArray[1] == "X" && ticArray[1] == ticArray[4] && ticArray[4] == ticArray[7]) 
+					|| (ticArray[2] == "X" && ticArray[2] == ticArray[5] && ticArray[5] == ticArray[8])
+					|| (ticArray[0] == "X" && ticArray[0] == ticArray[4] && ticArray[4] == ticArray[8]) 
+					|| (ticArray[2] == "X" && ticArray[2] == ticArray[4] && ticArray[4] == ticArray[6]))
 			{
 				JOptionPane.showMessageDialog(null, "Player A Wins!");
 				break;
@@ -67,8 +73,10 @@ public class TicTacToe
 			}
 			
 			// Player B input dialog message
-			outputMessageB = String.format("Let's play tic-tac-toe\n%s   %s   %s\n%s   %s   %s\n%s   %s   %s\nPlayer B:",ticArray[0], ticArray[1], ticArray[2],
-					ticArray[3], ticArray[4], ticArray[5], ticArray[6], ticArray[7], ticArray[8]);
+			outputMessageB = String.format("Let's play tic-tac-toe\n%s   %s   %s\n%s   %s   %s\n%s   %s   %s\nPlayer B:", 
+					ticArray[0], ticArray[1], ticArray[2],
+					ticArray[3], ticArray[4], ticArray[5], 
+					ticArray[6], ticArray[7], ticArray[8]);
 			
 			playerBChoice = JOptionPane.showInputDialog(null, outputMessageB);
 			playCount++;
@@ -96,10 +104,14 @@ public class TicTacToe
 			}
 
 			// Check to see if player B is a winner
-			if((ticArray[0] == "O" && ticArray[0] == ticArray[1] && ticArray[1] == ticArray[2]) || (ticArray[3] == "O" && ticArray[3] == ticArray[4] && ticArray[4] == ticArray[5]) 
-					|| (ticArray[6] == "O" && ticArray[6] == ticArray[7] && ticArray[7] == ticArray[8]) || (ticArray[0] == "O" && ticArray[0] == ticArray[3] && ticArray[3] == ticArray[6]) 
-					|| (ticArray[1] == "O" && ticArray[1] == ticArray[4] && ticArray[4] == ticArray[7]) || (ticArray[2] == "O" && ticArray[2] == ticArray[5] && ticArray[5] == ticArray[8])
-					|| (ticArray[0] == "O" && ticArray[0] == ticArray[4] && ticArray[4] == ticArray[8]) || (ticArray[2] == "O" && ticArray[2] == ticArray[4] && ticArray[4] == ticArray[6]))
+			if((ticArray[0] == "O" && ticArray[0] == ticArray[1] && ticArray[1] == ticArray[2]) 
+					|| (ticArray[3] == "O" && ticArray[3] == ticArray[4] && ticArray[4] == ticArray[5]) 
+					|| (ticArray[6] == "O" && ticArray[6] == ticArray[7] && ticArray[7] == ticArray[8]) 
+					|| (ticArray[0] == "O" && ticArray[0] == ticArray[3] && ticArray[3] == ticArray[6]) 
+					|| (ticArray[1] == "O" && ticArray[1] == ticArray[4] && ticArray[4] == ticArray[7]) 
+					|| (ticArray[2] == "O" && ticArray[2] == ticArray[5] && ticArray[5] == ticArray[8])
+					|| (ticArray[0] == "O" && ticArray[0] == ticArray[4] && ticArray[4] == ticArray[8]) 
+					|| (ticArray[2] == "O" && ticArray[2] == ticArray[4] && ticArray[4] == ticArray[6]))
 			{
 				JOptionPane.showMessageDialog(null, "Player B Wins!");
 				playAgain = false;
