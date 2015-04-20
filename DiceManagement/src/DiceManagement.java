7019ee047dc6e6566b790df69b0f68c2c0e2706f
Juan P Hernandez <juanphdez@gmail.com>
2015-04-19 20:13:37 -0700

    HW Week 09

--- DiceManagement/src/DiceManagement.java
+++ DiceManagement/src/DiceManagement.java
@@ -15,67 +15,32 @@ public class DiceManagement {
 		float winPercentage = 0;
 		float playerEven = 0;
 		float oddPercentage = 0;
-		String playerChoice;
+		String playerChoice = " ";
 		String answer;
-		String outputMessage;
+		
+		DiceNumber myDiceManagement = new DiceNumber(computerNum);
 		
 		// Process
 		do
 		{
-			JOptionPane.showMessageDialog(null, "Player throw the dice and guess wether it will be even or odd!");
-			computerNum = 1 + (int) (Math.random() * 6);
-			
-			playerChoice = JOptionPane.showInputDialog("What do you think?\nEnter even or odd: ");
-			
-			if ( playerChoice.equalsIgnoreCase("even") && (computerNum % 2 == 0))
-			{
-				JOptionPane.showMessageDialog(null, "Correct, the throw was even!");
-				gamesPlayed++;
-				playerWins++;
-				evenGuesses++;
-			}
-			else if ( playerChoice.equalsIgnoreCase("odd") && (computerNum % 2 != 0))
-			{
-				JOptionPane.showMessageDialog(null, "Correct, the throw was odd!");
-				gamesPlayed++;
-				playerWins++;
-				computerOdd++;
-			}
-			
-			else if ( playerChoice.equalsIgnoreCase("odd") && (computerNum % 2 == 0))
-			{
-				JOptionPane.showMessageDialog(null, "You lose, the throw was even!");
-				gamesPlayed++;
-			}
-			else if ( playerChoice.equalsIgnoreCase("even") && (computerNum % 2 != 0))
-			{
-				JOptionPane.showMessageDialog(null, "You lose, the throw was odd!");
-				gamesPlayed++;
-				evenGuesses++;
-				computerOdd++;
-			}
-			
-			else 
-				JOptionPane.showMessageDialog(null, "Invalid choice, try again.");
+			myDiceManagement.welcomeMessage();
+			computerNum = myDiceManagement.getRandomNumber();	
+			playerChoice = myDiceManagement.userInputMessage();
+			myDiceManagement.winLoseMessage();
 			
 			// Debug output to make sure it's correct.
 			System.out.println(playerChoice);
 			System.out.println(computerNum);
 			
-			answer = JOptionPane.showInputDialog("Do you want to play again, Yes or No?: ");
-			
+			answer = myDiceManagement.playAgain();		
 			
 		}while(answer.equalsIgnoreCase("yes"));
-		
-		winPercentage = ((float) playerWins / gamesPlayed) * 100;
-		playerEven = ((float) evenGuesses / gamesPlayed) * 100;
-		oddPercentage = ((float) computerOdd / gamesPlayed) * 100;
-		
-		outputMessage = String.format("Total games played: %d\nPlayer win percentage: %.0f%%\nPlayer guessed even %.0f%% of the time"
-				+ "\nDice rolled odd numbers %.0f%% of the time", gamesPlayed, winPercentage, playerEven, oddPercentage);
-
-		JOptionPane.showMessageDialog(null, outputMessage);
-		
+			
+		winPercentage = myDiceManagement.winPercentage(playerWins, gamesPlayed);
+		playerEven = myDiceManagement.playerEven(evenGuesses, gamesPlayed);
+		oddPercentage = myDiceManagement.oddPercentage(computerOdd, gamesPlayed);
+		gamesPlayed = myDiceManagement.gamesPlayed();
+		myDiceManagement.displayMessage(gamesPlayed, winPercentage, playerEven, oddPercentage);
 	}
 
-}//
+}
